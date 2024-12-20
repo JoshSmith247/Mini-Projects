d42af5d4755d31907e098d2bd00b112fcc633f46
Josh S <54862229+JoshSmith247@users.noreply.github.com>
2024-12-20 15:00:16 -0600

    Update dinner.py

--- Dinner-Party/dinner.py
+++ Dinner-Party/dinner.py
@@ -1,16 +1,32 @@
 import time
 import os
+import random
 
 
-password = "10010"
+password = "stardust girl"
 upass = ""
 
 while upass != password:
-    upass = input("What's the password? ")
+    upass = input("What's the password? ") # Make advanced-looking lock setup with print statements
     if upass != password:
         print("Incorrect password.")
 
+time.sleep(1)
+
 print("Correct Password! Let's start the game (;")
+
+time.sleep(2)
+
+os.system('clear')
+
+def prints(toprint):
+
+    for char in toprint:
+        print(char, end="", flush=True)
+        time.sleep(0.07)
+
+    
+    print("")
 
 # Start Game
 
@@ -29,89 +45,158 @@ while i < len(word):
     i += 1
 
 def render(limbs):
+    os.system('clear')
     if limbs == 0:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     Ô ")
-        print(" |       ")
-        print(" |       ")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     Ô ")
+        prints(" |       ")
+        prints(" |       ")
+        prints("__________")
     elif limbs == 1:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     0 ")
-        print(" |       ")
-        print(" |       ")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     0 ")
+        prints(" |       ")
+        prints(" |       ")
+        prints("__________")
     elif limbs == 2:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     0 ")
-        print(" |     | ")
-        print(" |       ")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     0 ")
+        prints(" |     | ")
+        prints(" |       ")
+        prints("__________")
     elif limbs == 3:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     0 ")
-        print(" |     | ")
-        print(" |    /  ")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     0 ")
+        prints(" |     | ")
+        prints(" |    /  ")
+        prints("__________")
     elif limbs == 4:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     0 ")
-        print(" |     | ")
-        print(" |    / | ")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     0 ")
+        prints(" |     | ")
+        prints(" |    / | ")
+        prints("__________")
     elif limbs == 5:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     0 ")
-        print(" |    /| ")
-        print(" |    / | ")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     0 ")
+        prints(" |    /| ")
+        prints(" |    / | ")
+        prints("__________")
     elif limbs == 6:
-        print("   ____  ")
-        print("  /    | ")
-        print(" |     0 ")
-        print(" |    /|l")
-        print(" |    / |")
-        print("__________")
+        prints("   ____  ")
+        prints("  /    | ")
+        prints(" |     0 ")
+        prints(" |    /|l")
+        prints(" |    / |")
+        prints("__________")
         time.sleep(0.1)
-        print("He gay??")
+        prints("He gay??")
         time.sleep(1)
         os.system('clear')
         time.sleep(1)
+        return
     
-    print("\nWord so far: ")
+    prints("\nWord so far: ")
 
     output = ""
     for l in uncovered:
         output += l + " "
     
-    print(output)
+    prints(output)
 
+# Game End
+
+def success():
+    os.system('clear')
+    i = 0
+
+    messagestr = "Congratulations on 20 amazing years, Stardust Girl!"
+    message = list(messagestr)
+
+    # Fancy Number Thing (S1)
+    while i < 100:
+
+        output = ""
+        j = 0
+
+        m = 0
+        k = (100 - i - j) / 2
+        while m < k:
+            output += " "
+            m += 1
+            
+        while j < i:
+
+            if random.randint(0, 100) < 1 and len(message) > 0:
+                output += "\033[1m" + message[0] + "\033[0m"
+                message.pop(0)
+            else:
+                output += str(random.randint(0, 1))
+
+            j += 1
+        prints(output)
+        time.sleep(0.05)
+
+        i += 1
+    
+    # Fancy Number Thing (S2)
+    while len(message) > 0:
+        output = ""
+        j = 0
+
+        # Randomly choose if this is a row that will contain a point
+
+
+        while j < 100:
+            if random.randint(0, 50) < 1:
+                output += message[0]
+                message.pop(0)
+            else:
+                output += str(random.randint(0, 1))
+            
+            j += 1
+        prints(output)
+        time.sleep(0.05)
+
+        i += 1
+    
+
+
+#success()
 # Round Method
 
 def round(limbs):
     render(limbs)
 
+    if limbs == 6:
+        return
+
     move = "null"
     while len(move) > 1 or len(move) < 1:
-        move = input("Choose a letter! ")
+        prints("Choose a letter! ")
+        move = input("")
         if len(move) > 1 or len(move) < 1:
-            print("Please choose a letter.")
+            prints("Please choose a letter.")
     
-    if move in word:
-        print("Congratulations! " + move + " is a letter in the word(s).")
+    if move in word or move.upper() in word or move.lower() in word:
+        prints("Congratulations! " + move + " is a letter in the word(s).")
 
         for l in word:
-            if l == move:
+            if l == move or l.lower() == move or l.upper() == move:
                 uncovered[word.index(l)] = l
                 word[word.index(l)] = "_"
+
+                if "_" not in uncovered:
+                    success()
+                    return
     else:
-        print("Sorry! " + move + " is not a letter in the word(s).")
+        prints("Sorry! " + move + " is not a letter in the word(s).")
         limbs += 1
     
     round(limbs)
